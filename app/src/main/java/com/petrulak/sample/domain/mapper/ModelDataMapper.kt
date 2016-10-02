ee2a778ae2316d748d36890f03e1948c7875cda8
Martin Petrulak <mato.petrulak@gmail.com>
2016-10-02 11:31:09 +0200

    Simplify model data mapper.

--- app/src/main/java/com/petrulak/sample/domain/mapper/ModelDataMapper.kt
+++ app/src/main/java/com/petrulak/sample/domain/mapper/ModelDataMapper.kt
@@ -3,16 +3,11 @@ package com.petrulak.sample.domain.mapper
 import com.petrulak.sample.data.model.ApiResponse
 import com.petrulak.sample.domain.model.ModelData
 import com.petrulak.sample.util.extensions.toFormattedDateString
-import java.util.*
 
 class ModelDataMapper {
 
   fun mapToModel(input: ApiResponse): List<ModelData> {
-    if (input != null && input.listValues != null) {
-      return input.listValues.map { item -> ModelData(item.dateAsLong.toFormattedDateString(), item.price.toInt()) }.orEmpty()
-    } else {
-      return Collections.emptyList()
-    }
+    return input.listValues.map { i -> ModelData(i.dateAsLong.toFormattedDateString(), i.price.toInt()) }.orEmpty()
   }
 
 }
